03b0b8ff87c07851dda798f8c15fe397c1a19a9c
Ana Cezanoski <juliacezanoski@gmail.com>
2024-04-01 18:41:11 -0300

    tentativa login e user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,45 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  showSuccessMessages = false;
+  showErrorMessages = false;
 
-  constructor(private router: Router) { }
+  formGroup = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    senha: new FormControl('', [Validators.required, Validators.pattern('^(?=.*\d).{8,}$')])
+  });
 
-  login() {    
-    this.router.navigate(['/layout/estoque']);
+  constructor(private router: Router) { } 
+
+  salvar(): void {
+    console.log('Salvando Usuário');
+    console.log(this.formGroup.controls.login.invalid);
+    console.log(this.formGroup.controls.login.touched); 
+    console.log(this.formGroup.controls.senha.invalid);
+    console.log(this.formGroup.controls.senha.touched);    
+
+    if (this.formGroup.invalid) {
+      console.log('Login Inválido!');
+      this.formGroup.markAllAsTouched();
+      this.showErrorMessages = true;
+      return;
+    }
+    console.log('Login efetuado!');
+    console.log('Login: ' + this.formGroup.controls.login.value);
+    console.log('Senha: ' + this.formGroup.controls.senha.value);
+  
+    console.log('Login válido');
+    this.showSuccessMessages = true;
   }
 
+  login() {
+    this.router.navigate(['/layout/jogo']);
+  }
 }
--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
- 
+
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrl: './usuario.component.css'
+  styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent {
 
@@ -12,51 +12,45 @@ export class UsuarioComponent {
   showErrorMessages = false;
 
   formGroup = new FormGroup({
-
     nome: new FormControl('', [Validators.required]),
     dtNasc: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     telefone: new FormControl('', [Validators.required]),
-    cpf: new FormControl('', [Validators.required]),
-    endereco: new FormControl('', [Validators.required]),
+    cpf: new FormControl('', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]), // Adicionando validação de padrão para CPF
+    endereco: new FormControl('', [Validators.required])
   });
-
 
   constructor() { }
 
-    ngOnInit(): void { }
+  ngOnInit(): void { }
 
-    salvar(): void {
-      console.log('Salvando Usuário');
+  salvar(): void {
+    console.log('Salvando Usuário')
+    console.log(this.formGroup.controls.nome.invalid);
+    console.log(this.formGroup.controls.nome.touched); 
+    console.log(this.formGroup.controls.dtNasc.invalid);
+    console.log(this.formGroup.controls.dtNasc.touched); 
+    console.log(this.formGroup.controls.email.invalid);
+    console.log(this.formGroup.controls.email.touched);   
+    console.log(this.formGroup.controls.telefone.invalid);
+    console.log(this.formGroup.controls.telefone.touched);   
+    console.log(this.formGroup.controls.cpf.invalid);
+    console.log(this.formGroup.controls.cpf.touched);
 
-      console.log(this.formGroup.controls.nome.invalid);
-      console.log(this.formGroup.controls.nome.touched); 
-      console.log(this.formGroup.controls.dtNasc.invalid);
-      console.log(this.formGroup.controls.dtNasc.touched); 
-      console.log(this.formGroup.controls.email.invalid);
-      console.log(this.formGroup.controls.email.touched);   
-      console.log(this.formGroup.controls.telefone.invalid);
-      console.log(this.formGroup.controls.telefone.touched);   
-      console.log(this.formGroup.controls.cpf.invalid);
-      console.log(this.formGroup.controls.cpf.touched);
-      console.log(this.formGroup.controls.endereco.invalid);
-      console.log(this.formGroup.controls.endereco.touched);
-
-      if (this.formGroup.invalid) {
-        console.log('Formulário Inválido!')
-        this.formGroup.markAllAsTouched();
-        this.showErrorMessages = true;
-        return;
-      }
-
-      console.log('Nome: ' + this.formGroup.controls.nome.value);
-      console.log('Data de Nascimento: ' + this.formGroup.controls.dtNasc.value);
-      console.log('E-mail: ' + this.formGroup.controls.email.value);
-      console.log('Telefone: ' + this.formGroup.controls.telefone.value);
-      console.log('CPF: ' + this.formGroup.controls.cpf.value);
-      console.log('Endereço: ' + this.formGroup.controls.endereco.value);
-    
-      console.log('Formulário válido');
-      this.showSuccessMessages = true;
+    if (this.formGroup.invalid) {
+      console.log('Formulário Inválido!');
+      this.showErrorMessages = true;
+      return;
     }
+
+    console.log('Salvando usuário!');
+    console.log('Nome: ' + this.formGroup.controls.nome.value);
+    console.log('Data de Nascimento: ' + this.formGroup.controls.dtNasc.value);
+    console.log('E-mail: ' + this.formGroup.controls.email.value);
+    console.log('Telefone: ' + this.formGroup.controls.telefone.value);
+    console.log('CPF: ' + this.formGroup.controls.cpf.value);
+
+    console.log('Formulário válido');
+    this.showSuccessMessages = true;
+  }
 }

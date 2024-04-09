65b0ffe9d4f101ce5a38058fbac914d51d0f2b43
Lucas <lucas221nascimento@gmail.com>
2024-04-09 02:30:08 -0300

    Problema de CSS resolvido
    Talvez seja preciso excluir as pastas .angular e node_modules e fazer npm install novamente, porque mudei todo o nome do projeto para BoardLend

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Puc2024web
+# BoardLend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, puc2024web');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BoardLend');
   });
 });
